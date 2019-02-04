b3a6810d20a777c1acac9605aaba1c3d50072ffd
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-02-04 20:44:06 +0500

    AnalyzeFBAnalytics.py

---
+++ AnalyzeFBAnalytics.py
@@ -0,0 +1,33 @@
+#from numpy import *
+from matplotlib import pyplot as plt
+import random
+
+with open('nm.csv', 'r') as fd:
+    data = fd.readlines()
+
+info = data[:2]
+lines = data
+data = []
+for line in lines:
+    words = line.split('\t')
+    data.append(words)
+
+
+def get_category(index):
+    values = []
+    for i in data:
+        values.append(i[index])
+    # category name, category explanation, category values
+    return (values[0], values[1], values[2:])
+
+m, d, x = get_category(random.randint(0, len(data[0])))
+X=[]
+for i in x:
+    if i == '':
+       X.append(0)
+    else: X.append(int(i))
+#plt.plot(X)
+plt.plot(X)
+plt.ylabel(m)
+plt.xlabel('days')
+plt.show()
